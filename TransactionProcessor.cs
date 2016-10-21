0244da9d77b8679ed1468c7029cfb3a4ba04beb5
Mayorov Maxim <nub.pun@yandex.ru>
2016-10-21 13:21:32 +0500

    first task delegate

---
+++ TransactionProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace work
+{
+    public abstract class TransactionProcessor
+    {
+        public Transaction Process(TransactionRequest request, CheckHandler Check, RegisterHandler Register, SaveHandler Save)
+        {
+
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+        public delegate bool CheckHandler(TransactionRequest request);
+        public delegate Transaction RegisterHandler(TransactionRequest request);
+        public delegate void SaveHandler(Transaction transaction);
+        //protected abstract bool Check(TransactionRequest request);
+        //protected abstract Transaction Register(TransactionRequest request);
+        //protected abstract void Save(Transaction transaction);
+    }
+
+   
+    public class Transaction
+    {
+    }
+
+    public class TransactionRequest {
+    }
+}
