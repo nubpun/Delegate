b310d7a1c63ffebb3b8839d7eb914a4675f094a2
Mayorov Maxim <nub.pun@yandex.ru>
2016-10-27 20:53:52 +0500

    fix

--- TransactionProcessor.cs
+++ TransactionProcessor.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace work
+namespace observ
 {
     public abstract class TransactionProcessor
     {
+        //Handler<Transaction, T>
         
         public Transaction Process<TRequest>(TRequest request, Handler<bool, TRequest> Check, Handler<Transaction, TRequest> Register, SaveHandler Save)
         {
-
             if (!Check(request))
                 throw new ArgumentException();
             var result = Register(request);
--- View.cs
+++ View.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace work
+namespace observ
 {
     class View : IObserver
     {
